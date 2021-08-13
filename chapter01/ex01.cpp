5cd993417c46748f237783452c4062e376940ef1
jeongseok1 <jeongseok.git@gmail.com>
2021-08-13 11:14:42 +0900

    ex01.cpp 추가

---
+++ chapter01/ex01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    cout << "hello world" << endl;
+    // std::cout << "hello world" << std::endl; -> using namespace std; 안쓸때
+
+    return 0;   // 파이썬 : sys.exit(0) 과 같음
+}
